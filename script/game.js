418c5de5f615072dc13c273f9aff9e9964059a5c
JIeHb <gribak98@list.ru>
2017-07-11 03:42:18 +1000

    Updated game.js

--- script/game.js
+++ script/game.js
@@ -1,4 +1,20 @@
+
 var size = 8;
+var img_types = ["straight.png", "angle.png", "v_sect.png", "empty.png"];
+var angles = [0, 90, 180, 360];
+
+var types = [[0, 1, 0, 1], [0, 0, 1, 1], [1, 2, 1, 2], [0, 0, 0, 0]]; // top, right, bottom, left in each array
+
+var random = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var cell = function(type, angle){
+	this.type = type;
+	this.angle = angle;
+}
+
+// Creating board //
 
 for(let i = 0; i < size; i++){
 	$("#game_board").append("<tr>");
@@ -8,14 +24,29 @@ for(let j = 0; j < size; j++){
 	$("tr").append("<td class = \"cell\">");
 }
 
-var random = function (min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+var board = [];
+for(let i = 0; i < size; i++){
+	board[i] = [];
+	for(let j = 0; j < size; j++){
+		board[i][j] = new cell(img_types[random(0, img_types.length - 1)], angles[random(0, angles.length)]);
+	}
 }
 
-var img_types = ["straight.png", "angle.png", "v_sect.png", "empty.png"];
-var angles = [0, 90, 180, 360];
+// Interface functions //
 
-var types = [[0, 1, 0, 1], [0, 0, 1, 1], [1, 2, 1, 2], [0, 0, 0, 0]];
+$("button").click(function(){
+	let message = "";
+	message = check_up() ? "You won" : "Something is wrong";
+	setTimeout(function(){
+		alert(message);
+	}, 200);
+});
+
+$(".cell").click(function(){ // Graphic's rotate. Also changes rotation angle for future things
+	let index = $(".cell").index(this);
+	board[Math.floor(index / size)][index % size].angle += 90;
+	draw();
+});
 
 var draw = function(){
 	for(let i = 0; i < size; i++){
@@ -27,7 +58,14 @@ var draw = function(){
 	}
 }
 
-var rotate = function(type, angle){
+draw();
+
+// Stuff //
+
+// Real rotation function (just shifts array). For example, if your rotation degree is 90 and
+// it was angle [0, 0, 1, 1], then it's shifting one time and will be [1, 0, 0, 1] 
+
+var rotate = function(type, angle){ 
 	let wire = [];
 	for(let i = 0; i < 4; i++){
 		wire[i] = types[img_types.indexOf(type)][i];
@@ -40,69 +78,38 @@ var rotate = function(type, angle){
 	return wire;
 }
 
-var cell = function(type, angle){
-	this.type = type;
-	this.angle = angle;
-}
-
-var board = [];
-for(let i = 0; i < size; i++){
-	board[i] = [];
-	for(let j = 0; j < size; j++){
-		board[i][j] = new cell(img_types[random(0, img_types.length - 1)], angles[random(0, angles.length)]);
+var set_color = function(type, angle){
+	let img;
+	switch(type){
+		case(img_types[0]): img = "u_line.png"; break;
+		case(img_types[1]): img = "u_ang.png"; break;
+		case(img_types[2]): img = "u_sect.png"; break;
 	}
+	let table_cell = $(".cell:eq(" +  (size * i + j) + ")");
+	table_cell.css("background-image", "url(\"img/" + img + "\")");
+	table_cell.css("transform", "rotate(" + angle + "deg)");
 }
-
-draw();
-
-$(".cell").click(function(){
-	let index = $(".cell").index(this);
-	board[Math.floor(index / size)][index % size].angle += 90;
-	let peace = board[Math.floor(index / size)][index % size];
-	draw();
-});
 
 var check_up = function(){
 	let i = 0; let j = 0;
 	let is_end = false;
 	let is_ok = false;
-	let img = "";
 	let moves_ = [[-1, 0], [0, 1], [1, 0], [0, -1]];
-	let table_cell = $(".cell:eq(" +  (size * i + j) + ")");
-	switch(board[i][j].type){
-		case(img_types[0]): img = "u_line.png"; break;
-		case(img_types[1]): img = "u_ang.png"; break;
-		case(img_types[2]): img = "u_sect.png"; break;
-	}
-	table_cell.css("background-image", "url(\"img/" + img + "\")");
-	table_cell.css("transform", "rotate(" + board[i][j].angle + "deg)");
+	set_color(board[i][j].type, board[i][j].angle);
 	let moves = rotate(board[i][j].type, board[i][j].angle);
-		if(moves[0]){
-			from = 0;
-		} else return false;
+	if(moves[0]){
+		from = 0;
+	} else return false;
 	while(!is_end){
 		moves = rotate(board[i][j].type, board[i][j].angle);
-		console.log(moves);
 		for(let k = 0; k < 4; k++){
 			if(i + moves_[k][0] < 0 || j + moves_[k][1] < 0 || i + moves_[k][0] > size - 1 || j + moves_[k][1] > size - 1) continue;
 			let next = board[i + moves_[k][0]][j + moves_[k][1]];
 			let next_moves = rotate(next.type, next.angle);
-			console.log("Next " + k + " " + next_moves);
-			console.log(next_moves[(k + 2) % next_moves.length] > 0);
-			console.log(from);
 			if(moves[k] > 0 && moves[k] == moves[from] && k != from && next_moves[(k + 2) % next_moves.length] > 0){
 				i += moves_[k][0];
 				j += moves_[k][1];
-				console.log(i + " " + j);				
-				let img = "";
-				let table_cell = $(".cell:eq(" +  (size * i + j) + ")");
-				switch(board[i][j].type){
-					case(img_types[0]): img = "u_line.png"; break;
-					case(img_types[1]): img = "u_ang.png"; break;
-					case(img_types[2]): img = "u_sect.png"; break;
-				}
-				table_cell.css("background-image", "url(\"img/" + img + "\")");
-				table_cell.css("transform", "rotate(" + board[i][j].angle + "deg)");
+				set_color(board[i][j].type, board[i][j].angle);
 				from = (k + 2) % 4;
 				moves = rotate(board[i][j].type, board[i][j].angle);
 				if(i == size - 1 && moves[2] && moves[2] == moves[from]) is_end = true;
@@ -117,13 +124,3 @@ var check_up = function(){
 	}
 	return is_end;
 }
-
-$("button").click(function(){
-	let message = "";
-	message = check_up() ? "You won" : "Something is wrong";
-	setTimeout(function(){
-		alert(message);
-	}, 200);
-});
-
-console.log("I'm still alive");
