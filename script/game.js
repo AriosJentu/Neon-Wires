732635ee32842c37be7d0ae8979d619ee8e8bc76
RMNTC <roman-vvvv@mail.ru>
2017-07-10 21:00:53 +1000

    Basic view game

--- script/game.js
+++ script/game.js
@@ -1,68 +1,58 @@
+var size = 8;
 
-var cell_ = {
-	top: 0,
-	right: 0,
-	bottom: 0,
-	left: 0,
-	angle: 0
-};
+for(let i = 0; i < size; i++){
+	$("#game_board").append("<tr>");
+}
+
+for(let j = 0; j < size; j++){
+	$("tr").append("<td class = \"cell\">");
+}
 
 var random = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-var types = ["straight.png", "angle.png"];
+var img_types = ["straight.png", "angle.png", "empty.png"];
 var angles = [0, 90, 180, 360];
+
+var types = [[0, 1, 0, 1], [0, 0, 1, 1], [0, 0, 0, 0]];
+console.log(types);
+
+var rotate = function(type, angle){
+	angle %= 360;
+	var index = img_types.indexOf(type);
+	var wire = types[index];
+	console.log(wire);
+	this.top = wire[0] * (angle == 90 || angle == 180);
+	this.right = wire[0] * (angle == 180 || angle == 270);
+	this.bottom = wire[0] * (angle == 270 || angle == 0);
+	this.left = wire[0] * (angle == 0 || angle == 90);
+}
+
 var cell = function(type, angle){
-	switch(type){
-		case("straight.png"):
-			this.top = 1;
-			this.right = 0;
-			this.bottom = 1;
-			this.left = 0;
-		break;
-		case("empty.png"):
-			this.top = 0;
-			this.right = 0;
-			this.bottom = 0;
-			this.left = 0;
-		break;
-		case("angle.png"):
-			this.top = 0;
-			this.right = 0;
-			this.bottom = 1;
-			this.left = 1;
-	}
 	this.type = type;
 	this.angle = angle;
 }
 
 var board = [];
-for(let i = 0; i < 10; i++){
+for(let i = 0; i < size; i++){
 	board[i] = [];
-	for(let j = 0; j < 10; j++){
-		board[i][j] = new cell(types[random(0, 2)], angles[random(0, 4)]);
+	for(let j = 0; j < size; j++){
+		board[i][j] = new cell(img_types[random(0, 2)], angles[random(0, 4)]);
 	}
-}
-
-for(let i = 0; i < 10; i++){
-	$("#game_board").append("<tr>");
-}
-
-for(let j = 0; j < 10; j++){
-	$("tr").append("<td class = \"cell\">");
 }
 
 $(".cell").click(function(){
 	let index = $(".cell").index(this);
-	board[Math.floor(index / 10)][index % 10].angle += 90;
+	board[Math.floor(index / size)][index % size].angle += 90;
+	let peace = board[Math.floor(index / size)][index % size];
 	draw();
 });
 
 var draw = function(){
-	for(let i = 0; i < 10; i++){
-		for(let j = 0; j < 10; j++){
-			let table_cell = $(".cell:eq(" +  i + j + ")");
+	for(let i = 0; i < size; i++){
+		for(let j = 0; j < size; j++){
+			let table_cell = $(".cell:eq(" +  (size * i + j) + ")");
 			table_cell.css("background-image", "url(\"img/" + board[i][j].type + "\"");
 			table_cell.css("transform", "rotate(" + board[i][j].angle + "deg)");
 		}
