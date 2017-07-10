27ec2d5e659014ff76d4e4453717296cf9796766
JIeHb <gribak98@list.ru>
2017-07-11 01:15:54 +1000

    Added simple checker

--- script/game.js
+++ script/game.js
@@ -16,17 +16,28 @@ var img_types = ["straight.png", "angle.png", "empty.png"];
 var angles = [0, 90, 180, 360];
 
 var types = [[0, 1, 0, 1], [0, 0, 1, 1], [0, 0, 0, 0]];
-console.log(types);
+
+var draw = function(){
+	for(let i = 0; i < size; i++){
+		for(let j = 0; j < size; j++){
+			let table_cell = $(".cell:eq(" +  (size * i + j) + ")");
+			table_cell.css("background-image", "url(\"img/" + board[i][j].type + "\")");
+			table_cell.css("transform", "rotate(" + board[i][j].angle + "deg)");
+		}
+	}
+}
 
 var rotate = function(type, angle){
-	angle %= 360;
-	var index = img_types.indexOf(type);
-	var wire = types[index];
-	console.log(wire);
-	this.top = wire[0] * (angle == 90 || angle == 180);
-	this.right = wire[0] * (angle == 180 || angle == 270);
-	this.bottom = wire[0] * (angle == 270 || angle == 0);
-	this.left = wire[0] * (angle == 0 || angle == 90);
+	let wire = [];
+	for(let i = 0; i < 4; i++){
+		wire[i] = types[img_types.indexOf(type)][i];
+	}
+	angle /= 90;
+	for(let i = 0; i < angle; i++){
+		let temp = wire.pop();
+		wire.unshift(temp);
+	}
+	return wire;
 }
 
 var cell = function(type, angle){
@@ -42,23 +53,59 @@ for(let i = 0; i < size; i++){
 	}
 }
 
+draw();
+
 $(".cell").click(function(){
 	let index = $(".cell").index(this);
 	board[Math.floor(index / size)][index % size].angle += 90;
 	let peace = board[Math.floor(index / size)][index % size];
+	console.log(rotate(peace.type, peace.angle));
 	draw();
 });
 
-var draw = function(){
-	for(let i = 0; i < size; i++){
-		for(let j = 0; j < size; j++){
-			let table_cell = $(".cell:eq(" +  (size * i + j) + ")");
-			table_cell.css("background-image", "url(\"img/" + board[i][j].type + "\"");
-			table_cell.css("transform", "rotate(" + board[i][j].angle + "deg)");
+var check_up = function(){
+	let i = 0; let j = 0;
+	let is_end = false;
+	let from = [1, 0, 0, 0];
+	let moves_ = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+	let table_cell = $(".cell:eq(" +  (size * i + j) + ")");
+	let img = board[i][j].type == img_types[0] ? "u_line.png" : "u_ang.png";
+	table_cell.css("background-image", "url(\"img/" + img + "\")");
+	table_cell.css("transform", "rotate(" + board[i][j].angle + "deg)");
+	while(!is_end){
+		let moves = rotate(board[i][j].type, board[i][j].angle);
+		console.log(moves);
+		for(let k = 0; k < 4; k++){
+			if(i + moves_[k][0] < 0 || j + moves_[k][1] < 0 || i + moves_[k][0] > size - 1 || j + moves_[k][1] > size - 1) continue;
+			let next = board[i + moves_[k][0]][j + moves_[k][1]];
+			let next_moves = rotate(next.type, next.angle);
+			console.log("Next " + k + " " + next_moves);
+			console.log(next_moves[(k + 2) % next_moves.length] == 1);
+			if(moves[k] == 1 && from[k] != 1 && next_moves[(k + 2) % next_moves.length] == 1){
+				from.fill(0);
+				switch(k){
+					case(0): i--; break;
+					case(1): j++; break;
+					case(2): i++; break;
+					case(3): j--; break;
+				}
+				console.log(i + ' ' + j);				
+				if(i < 0 || j < 0) return false;
+				let table_cell = $(".cell:eq(" +  (size * i + j) + ")");
+				let img = board[i][j].type == img_types[0] ? "u_line.png" : "u_ang.png";
+				table_cell.css("background-image", "url(\"img/" + img + "\")");
+				table_cell.css("transform", "rotate(" + board[i][j].angle + "deg)");
+				from[(k + 2) % moves.length] = 1;
+				console.log("from " + from);
+				if(i == size - 1) is_end = true;
+				break;
+			}
 		}
 	}
 }
 
-draw();
-console.log(board);
+$("button").click(function(){
+	check_up();
+});
+
 console.log("I'm still alive");
