fb3aaab2342759f3743222a21f2ec0bfe91ec91b
RMNTC <roman-vvvv@mail.ru>
2017-07-14 23:53:28 +1000

    Added random level generation

---
+++ generator.js
@@ -0,0 +1,98 @@
+"use strict"
+
+var random = function (min, max) {
+	return Math.floor(Math.random() * (max - min)) + min
+}
+
+var Generator = {}
+
+Generator.set_size = function(){
+	let rand = random(8, 15)
+	this.size = rand
+	this.found = false
+}
+
+Generator.set_start = function(){
+	this.start = [random(1, this.size - 1), random(1, this.size - 1)]
+}
+
+var board = []
+
+Generator.set_board = function(){
+	board = []
+	for(let i = 0; i < this.size; i++){
+		board[i] = []
+		for(let j = 0; j < this.size; j++){
+			board[i][j] = 0
+		}
+	}
+}
+
+Generator.set_way_length = function(){
+	this.way_length = random(4 * this.size, Math.floor(0.5 * this.size * this.size))
+}
+
+var randomize_array = function(arr){
+	let array = []
+	for(let i = 0; i < arr.length; i++){
+		let k = i
+		array[k] = arr[k]
+	}
+	let steps = random(0, 8)
+	for(let i = 0; i < steps; i++){
+		let a = random(0, ways.length)
+		let b = random(0, ways.length)
+		let temp = array[a]
+		array[a] = array[b]
+		array[b] = temp
+	}
+	return array
+}
+
+var is_back = function(from, to){
+	return (from[0] + to[0] == 0 && from[1] + to[1] == 0)
+}
+
+var is_out_of_range = function(point, size){
+	return (point[0] < 0 || point[1] < 0 || point[0] == size || point[1] == size)
+}
+
+var ways = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+var ways_ = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
+
+Generator.generate_level = function(){
+	Generator.set_size()
+	Generator.set_start()
+	Generator.set_board()
+	Generator.set_way_length()
+	board[Generator.start[0]][Generator.start[1]] = 4
+	Generator.start = [Generator.start[0] - 1, Generator.start[1]]
+	generate(Generator.start, [1, 0, 0, 0], 1)
+}
+
+var generate = function(point, from, depth){
+	if(depth == Generator.way_length){
+		board[point[0]][point[1]] = 4
+		Generator.found = true
+	}
+	for(let to of randomize_array(ways_)){
+		if(Generator.found){
+			return
+		}
+		let index = to.indexOf(1)
+		let new_point = [point[0] + ways[index][0], point[1] + ways[index][1]]
+		if(from[(index + 2) % 4] == 1 || is_out_of_range(new_point, Generator.size) || board[new_point[0]][new_point[1]]){
+			continue
+		}
+		board[point[0]][point[1]] = (from[index] == 0) + 1
+		generate(new_point, to, depth + 1)
+		if(!Generator.found){
+			board[point[0]][point[1]] = 0
+		}
+	}
+}
+
+Generator.get_board = function(){
+	this.generate_level()
+	return board
+}
---
+++ table.js
@@ -0,0 +1,76 @@
+
+Table = { is_initializated: false }
+
+Table.initializate = function(width, height) {
+	if(Table.is_initializated){
+		$("td.cell").remove()
+		$("td.border").remove()
+		$("td.border_corner").remove()
+		$("tr.tr").remove()
+	}
+	for (let y = 0; y < height + 2; y++) {
+		$("#game_board").append('<tr class = "tr" id="'+(y-1)+'">')
+	}
+	
+	for (let x = 0; x < width + 2; x++) {
+		$(".tr").each(function(index, element){
+			let obj_class = "cell"
+			let obj_id = $(this).attr("id")
+			if(obj_id == -1 || obj_id == Board.height || x < 1 || x > width){
+				obj_class = "border"
+			}
+			$(this).append('<td id ="'+ (x - 1) + '"' + ' class = "'+ obj_class +'"/>')
+		})
+	}
+
+	let corners = []
+	corners[0] = 0
+	corners[1] = width + 1
+	corners[2] = 2 * height + 2 * width + 3
+	corners[3] = 2 * height + width + 2
+
+	$(".border").each(function(index) {
+		let id_this = $(this).attr("id")
+		if(corners.indexOf(index) != -1){
+			$(this).attr("class", "border_corner")
+			$(this).css("transform", "rotate(" + 90 * corners.indexOf(index) + "deg)")
+		} else if(id_this > -1 && id_this < width){
+			let angle = index < width + 1 ? 90 : 270
+			$(this).css("transform", "rotate(" + angle + "deg)")
+		} else if(id_this == width){
+			$(this).css("transform", "rotate(180deg)")
+		} 
+	})
+
+	Table.is_initializated = true
+}
+
+Table.draw = function(index, image, angle) {
+	image = 'url("img/' + image + '")'
+	angle = "rotate(" + angle + "deg)"
+	let cell = $(".cell:eq(" + index + ")")
+	cell.css("background-image", image)
+	cell.css("transform", angle)
+}
+
+Table.get_position = function(cell) {
+	position = {}
+	position.y = parseInt($(cell).parent().attr("id"), 10)
+	position.x = parseInt($(cell).attr("id"), 10)
+	return position
+}
+
+Levels_table = {}
+
+Levels_table.initializate = function(){
+	for(let i = 1; i <= levels.length; i++){
+		$("#level_board").append('<span class = "level" id = ' + i + '>Level ' + i + '</span>')
+		if(i % 3 == 0){
+			$("#level_board").append("<br>")
+		}
+	}
+	$("#level_board").append("<br>")
+	$("#level_board").append('<span class = "random_level" style = "cursor: pointer">Generate random</span>')
+	$("#level_board").append("<br>")
+	$("#level_board").append('<span id = "close_level_board" style = "color: #ff4500">Close</span>')
+}()
